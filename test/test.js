647155fc0641828ef7165f160ed02f12bfc7ec5f
Jonathan Skeate <skeate@gmail.com>
2014-09-10 10:28:17 -0400

    Tweak tests to auto-start express server and use webdriverio

--- test/test.js
+++ test/test.js
@@ -1,22 +1,35 @@
 var should = require('chai').should();
-
-// Set up Selenium & webdriver
-var webdriverjs = require('webdriverjs');
+var express = require('express');
+var path = require('path');
+var webdriverio = require('webdriverio');
 var wdjsSeleniumBundle = require('webdriverjs-selenium-bundle');
 
-var client = webdriverjs.remote({
-  desiredCapabilities: {
-    browserName: 'chrome'
-  }
-});
-
-client.use(wdjsSeleniumBundle({autostop: true}));
-
-client.init().url('http://localhost:8888/examples/index.html');
 
 describe('Leaflet.buffer', function(){
-  this.timeout(9999999);
+  var client;
+
+  before(function(done){
+    this.timeout(9000); // selenium/chrome take a while to start up
+    var app = express();
+    app.use(express.static(path.join(__dirname, '..')));
+    app.listen(8888, function(){
+      // Set up Selenium & webdriver
+      client = webdriverio.remote({
+        desiredCapabilities: {
+          browserName: 'chrome'
+        }
+      });
+      client.use(wdjsSeleniumBundle({autostop: true}));
+      client.init().url('http://localhost:8888/examples/index.html', done);
+    });
+  });
+
   it('should add a button', function(done){
     return client.getElementSize('.leaflet-draw-edit-buffer', done);
+  });
+
+  after(function(done){
+    client.end();
+    setTimeout(done, 1000);
   });
 });
